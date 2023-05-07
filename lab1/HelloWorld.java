7048b28e9b4dbcef3f31f1930f3555db3e69747d
hellobabyJohn <2418267886@qq.com>
2023-05-07 23:42:35 +0800

    first commit

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static <String> void main(String[] args) {
-        System.out.println("Hello world!Everybody ");
+        System.out.println("Hello world!Jack ");
 	}
 } 
